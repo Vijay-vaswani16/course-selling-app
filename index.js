a96a5286bbbb26a508ff69631e68c75729a0680f
Vijay-vaswani16 <vaswanivijay890@gmail.com>
2025-06-21 23:17:01 +0530

    feat(api): adding morgan and rate limiter

--- index.js
+++ index.js
@@ -2,8 +2,19 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const mongoose = require('mongoose');
+const morgan = require('morgan');
+const { rateLimit } = require('express-rate-limit'); 
 const userRoute = require('./src/routes/userRoute');
 const adminRoute = require('./src/routes/adminRoute');
+
+const limiter = rateLimit({
+        windowMs: 1000,
+        limit: 10000000,
+        standardHeaders: true,
+        legacyHeaders: false
+});
+
+app.use(limiter);
 
 app.use(cors());
 app.use(express.json());
@@ -16,6 +27,8 @@ mongoose.connect(process.env.mongoDB_connection_url)
 const port = process.env.port;
 const adminBaseUrl = process.env.adminBaseUrl;
 const userBaseUrl = process.env.userBaseUrl;
+
+app.use(morgan(":method :url :status :res[content-length] - :response-time ms"));
 
 app.use(userBaseUrl, userRoute);
 app.use(adminBaseUrl, adminRoute);
